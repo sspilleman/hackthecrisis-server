90470a647e7a156ffcca413fec4bab3d7e91e451
Sander Spilleman <sander@spilleman.nl>
2020-04-05 16:24:37 +0200

    file upload icw axios

--- tryouts/multipart-streaming-upload.ts
+++ tryouts/multipart-streaming-upload.ts
@@ -4,40 +4,53 @@ import axios, { AxiosResponse, AxiosError } from "axios";
 
 const file = "/Volumes/RED1/Downloads/aircraftDatabase.csv";
 
-const formData = {
-    fileName: uuidv5("spilleman.nl", uuidv5.DNS),
-    file: fs.createReadStream(file)
-};
+const FormData = require('form-data');
 
-axios.post("http://localhost:2222/api/file", fs.createReadStream(file), {
-    headers: {
-        'Content-Type': 'multipart/form-data'
-    }
-}).then((response: AxiosResponse) => {
-    console.log(response.statusText);
-    // if (err) { return console.error('upload failed:', err, httpResponse); }
-    // console.log('Upload successful!', body);
-}).catch((reason: AxiosError) => {
-    console.log("failed:", reason.message)
+const form = new FormData();
+form.append('my_field', 'file');
+form.append('my_file', fs.createReadStream(file));
+
+console.log("form.getHeaders()", form.getHeaders());
+
+axios.post("http://localhost:2222/api/file", form, {
+    maxContentLength: 200000000,
+    headers: form.getHeaders()
 });
 
-const axiosheaders_urlencoded = {
-    accept: 'application/json, text/plain, */*',
-    'content-type': 'application/x-www-form-urlencoded',
-    'user-agent': 'axios/0.19.2',
-    'content-length': '751',
-    host: 'localhost:2222',
-    connection: 'close'
-}
+// const formData = {
+//     fileName: uuidv5("spilleman.nl", uuidv5.DNS),
+//     file: fs.createReadStream(file)
+// };
 
-const axiosheaders_formdata = {
-    accept: 'application/json, text/plain, */*',
-    'content-type': 'multipart/form-data',
-    'user-agent': 'axios/0.19.2',
-    'content-length': '751',
-    host: 'localhost:2222',
-    connection: 'close'
-}
+// axios.post("http://localhost:2222/api/file", fs.createReadStream(file), {
+//     headers: {
+//         'Content-Type': 'multipart/form-data'
+//     }
+// }).then((response: AxiosResponse) => {
+//     console.log(response.statusText);
+//     // if (err) { return console.error('upload failed:', err, httpResponse); }
+//     // console.log('Upload successful!', body);
+// }).catch((reason: AxiosError) => {
+//     console.log("failed:", reason.message)
+// });
+
+// const axiosheaders_urlencoded = {
+//     accept: 'application/json, text/plain, */*',
+//     'content-type': 'application/x-www-form-urlencoded',
+//     'user-agent': 'axios/0.19.2',
+//     'content-length': '751',
+//     host: 'localhost:2222',
+//     connection: 'close'
+// }
+
+// const axiosheaders_formdata = {
+//     accept: 'application/json, text/plain, */*',
+//     'content-type': 'multipart/form-data',
+//     'user-agent': 'axios/0.19.2',
+//     'content-length': '751',
+//     host: 'localhost:2222',
+//     connection: 'close'
+// }
 
 // var request = require('request');
 // var formData = {
